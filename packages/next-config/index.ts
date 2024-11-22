b4fb13094e46e13deea5b2d4b1fafd9b92a13146
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2024-11-22 16:17:51 +0200

    chore: cleanups and tweaks
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- packages/next-config/index.ts
+++ packages/next-config/index.ts
@@ -1,7 +1,4 @@
 import withBundleAnalyzer from '@next/bundle-analyzer';
-
-// @ts-expect-error No declaration file
-import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 import { env } from '@repo/env';
 import { withSentryConfig } from '@sentry/nextjs';
 import withVercelToolbar from '@vercel/toolbar/plugins/next';
@@ -9,6 +6,10 @@ import type { NextConfig } from 'next';
 import { createSecureHeaders } from 'next-secure-headers';
 
 export const config: NextConfig = withVercelToolbar()({
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
@@ -19,23 +20,23 @@ export const config: NextConfig = withVercelToolbar()({
     ],
   },
 
-  // biome-ignore lint/suspicious/useAwait: rewrites is async
-  async rewrites() {
-    return [
-      {
-        source: '/ingest/static/:path*',
-        destination: 'https://us-assets.i.posthog.com/static/:path*',
-      },
-      {
-        source: '/ingest/:path*',
-        destination: 'https://us.i.posthog.com/:path*',
-      },
-      {
-        source: '/ingest/decide',
-        destination: 'https://us.i.posthog.com/decide',
-      },
-    ];
-  },
+  // xbiome-ignore lint/suspicious/useAwait: rewrites is async
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/ingest/static/:path*',
+  //       destination: 'https://us-assets.i.posthog.com/static/:path*',
+  //     },
+  //     {
+  //       source: '/ingest/:path*',
+  //       destination: 'https://us.i.posthog.com/:path*',
+  //     },
+  //     {
+  //       source: '/ingest/decide',
+  //       destination: 'https://us.i.posthog.com/decide',
+  //     },
+  //   ];
+  // },
 
   // biome-ignore lint/suspicious/useAwait: headers is async
   async headers() {
@@ -53,15 +54,15 @@ export const config: NextConfig = withVercelToolbar()({
     ];
   },
 
-  webpack(config, { isServer }) {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
+  // webpack(config, { isServer }) {
+  //   if (isServer) {
+  //     config.plugins = [...config.plugins, new PrismaPlugin()];
+  //   }
 
-    config.ignoreWarnings = [{ module: /@opentelemetry\/instrumentation/ }];
+  //   config.ignoreWarnings = [{ module: /@opentelemetry\/instrumentation/ }];
 
-    return config;
-  },
+  //   return config;
+  // },
 
   // This is required to support PostHog trailing slash API requests
   skipTrailingSlashRedirect: true,
